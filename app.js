08a57e53569f9c0aa9e7406e3d5902bcdbd81a8d
Fernando Suarez <jorgefernandosuarez@gmail.com>
2026-06-03 09:23:36 -0300

    feat: se crean las vistas de crear publicacion y detalle, se agregan unas rutas para probar las  vistas

--- app.js
+++ app.js
@@ -14,20 +14,34 @@ const app = express();
 //MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 
 //  MOTOR DE PLANTILLAS
 app.set('view engine', 'pug');
 app.set('views', './views');
-app.use(express.static('public'));
 
 
 //ROUTES
-app.get('/', (req,res) => {
-    res.send('hola');
 
+app.get('/', (req,res)=>{
+    res.render('home');
 })
 
+app.get('/signup', (req,res) => {
+    res.render('auth/signup');
+})
+
+app.get('/login', (req,res) => {
+    res.render('auth/login.pug')
+}) 
+app.get('/publicaciones/crear', (req,res)=>{
+    res.render('publicaciones/crear');
+})
+
+app.get('/publicaciones/:id', (req,res)=>{
+    res.render('publicaciones/detalle');
+})
 
 //CONEXION DB
     try {
