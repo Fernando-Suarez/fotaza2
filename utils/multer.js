8587e8d01261de9ddf4c5c8bfc5f842093796e70
Fernando Suarez <jorgefernandosuarez@gmail.com>
2026-06-04 02:57:57 -0300

    feat: se hace el archivo de configuracion del multer y se instalan las dependencias de  sharp y de express-session

---
+++ utils/multer.js
@@ -0,0 +1,13 @@
+//Multer
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, './public/imagenes');
+	},
+	filename: (req, file, cb) => {
+		cb(null, `${Date.now()}-${file.originalname}`);
+	},
+});
+
+export const upload = multer({ storage });
