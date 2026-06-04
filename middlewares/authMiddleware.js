c64a334cb3687652d224cb11e9a5fc86aff146be
Fernando Suarez <jorgefernandosuarez@gmail.com>
2026-06-04 12:14:47 -0300

    feat: se crean el middleware de autenticacion y un middleware para cuando ya está autenticado

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,40 @@
+import { Usuario } from "../models/Usuario";
+
+export async function authMiddleware(req,res,next){
+    const userId = req.session.user;
+    if(!userId){
+        res.redirect('/auth/login');
+        return;
+    }
+    try {
+        const user = await Usuario.findByPk(userId,{
+            attributes: ['id','nombre','email','rol','avatar']
+        });
+        if(!user){
+            req.session.destroy(()=>{});
+            res.redirect('/auth/login');
+            return;
+        }
+        res.locals.user = {
+            id: user.id,
+            nombre: user.nombre,
+            rol: user.rol,
+            avatar: user.avatar,
+        };
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.redirect('/auth/login');
+    }
+
+}
+
+
+export function autenticadoMiddleware(req,res,next){
+
+    if(req.session.user){
+        return res.redirect('/');
+    }
+
+    next();
+}
