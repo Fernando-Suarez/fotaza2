e62115fdbbf29b4b15abd24ceb3b4f8b148f60ae
Fernando Suarez <jorgefernandosuarez@gmail.com>
2026-06-04 12:18:24 -0300

    update: se actualizan las  funciones del login y las rutas

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -15,19 +15,36 @@ export const login = async (req,res)=>{
     try {
         const {email,password} = req.body;
         const validacion = validarLogin({email,password});
+        if(!validacion.success){
+            res.status(400).render('auth/login', {formValues: {email: email}});
+            return;
+        }
         const usuarioEncontrado = await usuarioPorEmail(email);
         if(!usuarioEncontrado){
-            res.status(400).render('auth/login', {formValues: {email: email, password:password}});
+            res.status(400).render('auth/login', {formValues: {email: email}});
             return;
         }
         const passValidado = await usuarioEncontrado.validatePass(password);
         if(!passValidado){
-            res.status(400).render('auth/login', {formValues: {email: email, password:password}});
-        } else{
-            res.status(200).render('home',{user:usuarioEncontrado});
+            res.status(400).render('auth/login', {formValues: {email: email}});
+            return;
         }
+
+            req.session.user = usuarioEncontrado.id;
+            return res.redirect('/');
+            //res.status(200).render('home',{user:usuarioEncontrado});
+        
     } catch (error) {
         console.log('post: login',error);
+        return res.redirect('/auth/login');
         
     }
+}
+
+export const logout = (req,res)=>{
+
+    req.session.destroy(()=>{
+
+        res.redirect('/auth/login');
+    });
 }
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,14 +1,16 @@
 import {Router} from 'express';
 import { getSignUpForm, signUp } from '../controllers/signUpController.js';
-import { getLoginForm,login } from '../controllers/loginController.js';
+import { getLoginForm,login, logout } from '../controllers/loginController.js';
+import { autenticadoMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
 
-router.get('/login', getLoginForm);
+router.get('/login',autenticadoMiddleware, getLoginForm);
 router.post('/login', login);
-router.get('/signup',getSignUpForm);
+router.get('/signup',autenticadoMiddleware,getSignUpForm);
 router.post('/signup', signUp);
+router.get('/logout', logout);
 
 
 export default router;
